33d9205365c55bf3472f66f1ed17b1c26c23ddd4
Ldsc2002 <luissantoscuellar@gmail.com>
2022-11-08 21:10:44 -0600

    WIP running game

--- PyGL/driver.py
+++ PyGL/driver.py
@@ -1,6 +1,5 @@
 from PyGL.lib.PyGL import *
 from random import *
-from copy import deepcopy
 
 GL = None
 
@@ -8,76 +7,29 @@ def checkInstanceOnCall(function):
     if GL is None: init()
     return function
 
-def init(width: int = 1000, height: int = 1000) -> None:
+def init(width: int = 1000, height: int = 500, pixelSize: int = 50) -> None:
     global GL
-    GL = PyGL(width, height)
+    GL = PyGL(width, height, pixelSize)
 
-def conwaysGameOfLife(pixelSize = 1, width = 250, height = 250, initialData = None):
-    init(width, height)
-    data = [[False for y in range(int(height / pixelSize))] for x in range(int(width / pixelSize))]
+@checkInstanceOnCall
+def runGame(file):
+    GL.loadMap(file)
 
-    if width % pixelSize != 0 or height % pixelSize != 0:
-        raise Exception("Pixel size must be a factor of width and height")
+    running = True 
 
-    if initialData:
-        for x, y in initialData:
-            data[x - 1][y - 1] = True
-    else:
-        for x in range(int(width / pixelSize)):
-            for y in range(int(height / pixelSize)):
-                if random() < 0.1:
-                    data[x][y] = True
-
-    GL.enableScissor(True)
-
-    running = True
     while running:
-        for x in range(len(data)):
-            for y in range(len(data[0])):
-                if data[x][y]:
-                    GL.pixel(x * pixelSize, y * pixelSize, (1.0, 1.0, 1.0, 1.0), pixelSize)
-                else:
-                    GL.pixel(x * pixelSize, y * pixelSize, (0.0, 0.0, 0.0, 1.0), pixelSize)
-
-        pygame.display.flip()
+        GL.render()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-                GL.enableScissor(False)
-        
-        newData = deepcopy(data)
-        for x in range(len(data)):
-            for y in range(len(data[0])):
-                neighbors = checkNeighbours(x, y, data)
-                
-                if data[x][y]:
-                    if neighbors < 2:
-                        newData[x][y] = False
 
-                    elif neighbors > 3:
-                        newData[x][y] = False
-
-                    elif neighbors == 2 or neighbors == 3:
-                        newData[x][y] = True
-                        
-                elif neighbors == 3:
-                    newData[x][y] = True
-        data = newData
-            
-def checkNeighbours(x, y, data):
-    neighbours = 0
-    for i in range(-1, 2):
-        for j in range(-1, 2):
-            if i == 0 and j == 0: continue
-            xOffset, yOffset = x + i, y + j
-
-            if xOffset >= len(data):
-                xOffset = 0
-
-            if yOffset >= len(data[0]):
-                yOffset = 0
-
-            if data[xOffset][yOffset]:
-                neighbours += 1
-    return neighbours
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    GL.player['a'] += pi/10
+                if event.key == pygame.K_LEFT:
+                    GL.player['a'] -= pi/10
+                if event.key == pygame.K_UP:
+                    GL.player['y'] += 10
+                if event.key == pygame.K_DOWN:
+                    GL.player['y'] -= 10
--- PyGL/lib/PyGL.py
+++ PyGL/lib/PyGL.py
@@ -1,20 +1,136 @@
 import pygame   
 from OpenGL.GL import *
+from math import cos, sin, pi
+
+WHITE = (1.0, 1.0, 1.0)
+BLACK = (0.0, 0.0, 0.0)
+RED = (1.0, 0.0, 0.0)
+GREEN = (0.0, 1.0, 0.0)
+BLUE = (0.0, 0.0, 1.0)
+YELLOW = (1.0, 1.0, 0.0)
+CYAN = (0.0, 1.0, 1.0)
+
+SKY = (40,100,200)
+GROUND = (200,200,100)
+
+walls = {
+    "1": pygame.image.load("PyGL/Assets/wall1.png"),
+    "2": pygame.image.load("PyGL/Assets/wall2.png"),
+    "3": pygame.image.load("PyGL/Assets/wall3.png"),
+    "4": pygame.image.load("PyGL/Assets/wall4.png"),
+    "5": pygame.image.load("PyGL/Assets/wall5.png"),
+}
 
 class PyGL:
-    def __init__(this, width, height):
+    def __init__(this, width, height, blockSize = 50):
         pygame.init()
-        this.width = width
-        this.height = height
-        this.screen = pygame.display.set_mode((width, height), pygame.OPENGL | pygame.DOUBLEBUF)
+        this.screen = pygame.display.set_mode((width, height))
+        _, _, this.width, this.height = this.screen.get_rect()
+        this.blockSize = blockSize
+        this.map = []
+        this.player = {
+            'x': int(this.blockSize + this.blockSize / 2),
+            'y': int(this.blockSize + this.blockSize / 2),
+            'fov': int(pi/3),
+            'a': int(pi/3),
+        }
 
-    def enableScissor(this, enable):
-        if enable:
-            glEnable(GL_SCISSOR_TEST)
-        else:
-            glDisable(GL_SCISSOR_TEST)
+    def pixel(this, x, y, color = WHITE):
+        this.screen.set_at((x, y), color)
 
-    def pixel(this, x, y, color, size = 1):
-        glScissor(x, y, size, size)
-        glClearColor(color[0], color[1], color[2], 1.0)
-        glClear(GL_COLOR_BUFFER_BIT)
+    def block(this, x, y, wall):
+        for i in range(x, x + this.blockSize):
+            for j in range(y, y + this.blockSize):
+                tx = int((i - x) * 128 / this.blockSize)
+                ty = int((j - y) * 128 / this.blockSize)
+
+                c = wall.get_at((tx,ty))
+                this.pixel(i, j, c)
+
+    def loadMap(this, file):
+        with open(file, "r") as f:
+            mapFile = f.read().splitlines()
+            
+            for line in mapFile:
+                this.map.append(list(line))
+
+    def drawMap(this):
+        for y in range(len(this.map)):
+            for x in range(len(this.map[y])):
+                if this.map[y][x] != " ":
+                    wall = this.map[y][x]
+                    this.block(x * this.blockSize, y * this.blockSize, walls[wall])
+
+    def drawPlayer(this):
+        this.pixel(this.player['x'],this.player['y'], WHITE)
+
+    def drawStake(this, x, h, c, tx):
+        start_y = int(this.height/2 - h/2)
+        end_y = int(this.height/2 + h/2)
+        height = end_y - start_y
+
+        for y in range(start_y, end_y):
+            ty = int((y - start_y) * 128 / height)
+            color = walls[c].get_at((tx,ty))
+            this.pixel(x, y, color)
+
+    def castRay(this, a = None):
+        d = 0
+        ox = this.player['x']
+        oy = this.player['y']
+        
+        while True:
+            x = int(ox + d * cos(a))
+            y = int(oy + d * sin(a))
+
+            i = int(x / this.blockSize)
+            j = int(y / this.blockSize)
+
+            if this.map[j][i] != ' ':
+                hitx = x - i * this.blockSize
+                hity = y - j * this.blockSize
+
+                if 1 < hitx < this.blockSize - 1:
+                    maxhit = hitx
+                else:
+                    maxhit = hity
+
+                tx = int(maxhit * 128 / this.blockSize)
+                return d, this.map[j][i], tx
+
+            this.pixel(x, y)
+            d += 1
+            
+    def render(this, density = 100):
+        this.screen.fill(BLACK, (0, 0, this.width, this.height))
+        this.screen.fill(SKY, (this.width / 2, 0, this.width, this.height / 2))
+        this.screen.fill(GROUND, (this.width / 2, this.height / 2, this.width, this.height / 2))
+
+        this.drawMap()
+        this.drawPlayer()
+
+        for i in range(0,density):
+            a = this.player['a'] - this.player['fov']/2 + this.player['fov'] * i/density
+            d, c, _ = this.castRay(a)
+
+        for i in range(0,500):
+            this.pixel(499, i, WHITE)
+            this.pixel(500, i, WHITE)
+            this.pixel(501, i, WHITE)
+
+        for i in range(0, int(this.width/2)):
+            a = this.player['a'] - this.player['fov'] / 2 + this.player['fov'] * i / (this.width / 2)
+            d,c,tx = this.castRay(a)
+
+            if (d == 0): d = 1
+            x = int(this.width/2) + i
+            h = this.height / (d * cos(a - this.player['a'])) * this.height / 10
+
+            this.drawStake(x,h,c,tx)
+
+        this.flip()
+
+    def flip(this):
+        pygame.display.flip()
+
+
